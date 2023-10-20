664209befe2580b87a52733c5b84d44560bca075
matthwchun <matthewchunemail@gmail.com>
2023-10-20 13:24:36 -0700

    updated main

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,5 @@
 #include "c-echo.h"
 
 int main(int argv, char** argc) {
-    std::cout << echo(argv, argc);
-    std::cout << "\n";
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
